92d87ead0d9d342da8936c4643a8b4ba1c68bb41
温海杰 <wenhaijie@light2cloud.com>
2020-04-16 21:28:01 +0800

    <Add> (Apps) add Startup script

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+function cleanup()
+{
+    local pids=`jobs -p`
+    if [[ "${pids}" != ""  ]]; then
+        kill ${pids} >/dev/null 2>/dev/null
+    fi
+}
+
+service="all"
+if [[ "$1" != "" ]];then
+    service=$1
+fi
+
+trap cleanup EXIT
+if [[ "$1" == "bash" ]];then
+    bash
+else
+    python3 run_server start ${service}
+fi
